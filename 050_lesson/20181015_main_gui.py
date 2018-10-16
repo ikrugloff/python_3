e900d30dcad18358041637ffb408074ba51deb24
ikrugloff <i9200418888@gmail.com>
2018-10-17 00:25:59 +0300

    Finished function "get_all_positions". Feature "add_new_position" were started.

--- 050_lesson/20181015_main_gui.py
+++ 050_lesson/20181015_main_gui.py
@@ -1,10 +1,11 @@
-# Использование функции loadUi()
+import datetime
+
 from PyQt5 import QtWidgets, uic
 import sys
 import time
 from PyQt5.QtWidgets import QListWidget
 
-import purchase_list
+from purchase_list import Purchase
 from PyQt5.uic.properties import QtCore
 
 app = QtWidgets.QApplication(sys.argv)
@@ -47,80 +48,117 @@ window = uic.loadUi('20181015_main_gui.ui')
 # window.pushButton_launch.clicked.connect(hello)
 #########
 
-# show GUI window
+
 window.show()
-# window.listWidget_satisfaction.show()
 
-window.comboBox_sex.addItem('Female')  # TODO: How do it in designer??? Right mouse button???
-window.comboBox_sex.addItem('Male')
-
+# window.comboBox_sex.addItem('Female')  # TODO: How do it in designer??? Right mouse button???
+# window.comboBox_sex.addItem('Male')
+purchase = Purchase()
 ######
-self.listWidget_extractedmeters.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
-self.listWidget_extractedmeters.connect(self.listWidget_extractedmeters, QtCore.SIGNAL("customContextMenuRequested(QPoint)" ), self.listItemRightClicked)
+"""
+Get all positions
+"""
+purchase_list = purchase.get_all_positions()
 
 
-def listItemRightClicked(self, QPos):
-    self.listMenu= QtGui.QMenu()
-    menu_item = self.listMenu.addAction("Remove Item")
-    self.connect(menu_item, QtCore.SIGNAL("triggered()"), self.menuItemClicked)
-    parentPosition = self.listWidget_extractedmeters.mapToGlobal(QtCore.QPoint(0, 0))
-    self.listMenu.move(parentPosition + QPos)
-    self.listMenu.show()
+def get_all_positions():
+    for purchase in purchase_list:
+        window.listWidget_purchase_list.addItem(f'{str(purchase["name"])} : {str(purchase["quantity"])} '
+                                                f'{str(purchase["unit_of_measurement"])} (Purchased date: '
+                                                f'{str(purchase["purchase_date"])})')
 
 
-def menuItemClicked(self):
-    currentItemName=str(self.listWidget_extractedmeters.currentItem().text() )
-    print(currentItemName)
+window.pushButton_refresh_purchase_list.clicked.connect(get_all_positions)
 ######
+"""
+Add new position
+"""
+def validate_data():
+    purchase_date = window.lineEdit_purchase_date.text()
+    try:
+        datetime.datetime.strptime(purchase_date, '%Y/%m/%d')
+        return True
+    except Exception as e:
+        return False
 
-#########
-#  Why does session start before clicking the button??????
-def interest_search():
-
-    session = vk.AuthSession(app_id=MyVkData.APP_ID, user_login=MyVkData.LOGIN,
-                             user_password=MyVkData.GET_PASSWORD())
-    vkapi = vk.API(session, timeout=100)
-
-    ######
-    # INTERESTS = ['путешествия', 'фильмы']
-    INTERESTS = window.lineEdit_for_1_interest.text()
-    ######
-
-    AGE_FROM = window.lineEdit_min_age.text()
-    AGE_TO = window.lineEdit_max_age.text()
-
-    ######
-    sex = str(window.comboBox_sex.currentText())
-    if sex == 'Female':
-        SEX = 1
+def line_edit_checking():
+    valid_data = validate_data()
+    if valid_data:
+        if window.lineEdit_name.text() is not None \
+                and window.lineEdit_quantity.text() is not None \
+                and window.lineEdit_unit_of_measurement.text() is not None \
+                and window.lineEdit_purchase_date.text() is not None:
+            return True
+        else:
+            return False
     else:
-        SEX = 2
-    ######
+        return False
 
-    CITY = 1  # Moscow
-
-    def timeout(query):
-        time.sleep(0.3)
-        return query
-
-    # Why does we need comma in "interests" when we use list as input??????
-    users = vkapi.users.search(interests=''.join(INTERESTS), city=CITY, sex=SEX, age_from=AGE_FROM,
-                               age_to=AGE_TO, fields='photo_big,domain', access_token=MyVkData.ACCESS_TOKEN_VK,
-                               v=MyVkData.API_VERSION_VK)
-
-    users = timeout(users)
-    ITEMS = 'items'
-    user_list = users[ITEMS]
+def add_new_position():
+    check = line_edit_checking()
+    if check:
+        my_dict = {
+            "name": window.lineEdit_name.text(),
+            "quantity": window.lineEdit_quantity.text(),
+            "unit_of_measurement": window.lineEdit_unit_of_measurement.text(),
+            "purchase_date": window.lineEdit_purchase_date.text(),
+            "status": 0
+        }
+        purchase.add_new_position(my_dict)
+    else:
+        pass
 
 
-    for user in user_list:
-        window.listWidget_satisfaction.addItem(f'{str(user["id"])}, {str(user["first_name"])}, '
-                                               f'{str(user["last_name"])}, {str(user["domain"])}')
-        # window.listWidget_satisfaction.addItem(str(user))
-    window.listWidget_satisfaction.addItem(INTERESTS)
-###
-window.pushButton_launch.clicked.connect(interest_search)
-#########
+window.pushButton_add_new_position.clicked.connect(add_new_position)
+
+# #########
+# #  Why does session start before clicking the button??????
+# def interest_search():
+#
+#     session = vk.AuthSession(app_id=MyVkData.APP_ID, user_login=MyVkData.LOGIN,
+#                              user_password=MyVkData.GET_PASSWORD())
+#     vkapi = vk.API(session, timeout=100)
+#
+#     ######
+#     # INTERESTS = ['путешествия', 'фильмы']
+#     INTERESTS = window.lineEdit_for_1_interest.text()
+#     ######
+#
+#     AGE_FROM = window.lineEdit_min_age.text()
+#     AGE_TO = window.lineEdit_max_age.text()
+#
+#     ######
+#     sex = str(window.comboBox_sex.currentText())
+#     if sex == 'Female':
+#         SEX = 1
+#     else:
+#         SEX = 2
+#     ######
+#
+#     CITY = 1  # Moscow
+#
+#     def timeout(query):
+#         time.sleep(0.3)
+#         return query
+#
+#     # Why does we need comma in "interests" when we use list as input??????
+#     users = vkapi.users.search(interests=''.join(INTERESTS), city=CITY, sex=SEX, age_from=AGE_FROM,
+#                                age_to=AGE_TO, fields='photo_big,domain', access_token=MyVkData.ACCESS_TOKEN_VK,
+#                                v=MyVkData.API_VERSION_VK)
+#
+#     users = timeout(users)
+#     ITEMS = 'items'
+#     user_list = users[ITEMS]
+#
+#
+#     for user in user_list:
+#         window.listWidget_satisfaction.addItem(f'{str(user["id"])}, {str(user["first_name"])}, '
+#                                                f'{str(user["last_name"])}, {str(user["domain"])}')
+#         # window.listWidget_satisfaction.addItem(str(user))
+#     window.listWidget_satisfaction.addItem(INTERESTS)  # check for entered interest
+# ###
+# window.pushButton_launch.clicked.connect(interest_search)
+# #########
 
 # start GUI app
 sys.exit(app.exec_())
